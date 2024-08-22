74bc1958cfc31606ac46ee7f3fb4e7fcb4262ba8
L-Marcel <lmgh1312@gmail.com>
2024-08-22 12:12:33 -0300

    Atualizando casa

--- components/environment/Home.gd
+++ components/environment/Home.gd
@@ -2,16 +2,20 @@ class_name Home
 extends StaticBody2D
 
 @export var layers : Array[TileMapLayer] = [];
+@export var sound_door : AudioStreamPlayer;
 var characters : Array[Character] = [];
 
+func update() -> void:
+	var new_visible : bool = characters.size() == 0;
+	var old_visible : bool = layers[0].visible;
+	if old_visible != new_visible:
+		for layer in layers: layer.visible = new_visible;
+	sound_door.play();
 func _on_zone_body_entered(body) -> void:
 	if body is Character:
 		characters.append(body);
-		for layer in layers:
-			layer.visible = characters.size() == 0;
-
+		update();
 func _on_zone_body_exited(body) -> void:
 	if body is Character:
 		characters.erase(body);
-		for layer in layers:
-			layer.visible = characters.size() == 0;
+		update();
